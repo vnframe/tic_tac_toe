315a1710fe8b41d425e0fb093f9bc90fcbb85184
vnframe <vnframe95@icloud.com>
2017-10-02 12:54:01 -0400

    sequential working

--- console_game.rb
+++ console_game.rb
@@ -1,5 +1,6 @@
 require_relative "tictactoe.rb"
 require_relative "console_human.rb"
+require_relative "console_sequential.rb"
 class ConsoleGame
     attr_accessor :player_1, :player_2, :board, :active_player, :move, :input1, :input2
     def initialize
