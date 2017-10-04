3f44ebb36c463fead0a34b01309cd4d26b7ef727
vnframe <vnframe95@icloud.com>
2017-10-04 11:36:15 -0400

    got a block function working

--- unbeatable.rb
+++ unbeatable.rb
@@ -1,10 +1,19 @@
 class Unbeatable
-    attr_accessor :marker
+    attr_accessor :marker, :opponent
     def initialize(marker)
         @marker = marker
+        @opponent = get_opponent(@marker)
     end
-    def get_move(ttt_board)
-
+    def get_opponent(marker)
+            opponent = 'X'
+    
+            if marker == 'X'
+                opponent = 'O'
+            else
+                opponent = 'X'
+            end        
+    end
+    def win_block_move(ttt_board, player)
         possible_moves = [[ttt_board[0],ttt_board[1], ttt_board[2]], [ttt_board[3], ttt_board[4], ttt_board[5]]]
         possible_combos = [
             [0, 1, 2],
@@ -22,13 +31,26 @@ class Unbeatable
         index=0
         possible_moves.each_with_index do |wins, index|
             #p "#{wins} are here"
-            if wins.count(marker) == 2 && wins.include?('')
+            if wins.count(player) == 2 && wins.include?('')
                 p index
                 winner = wins.index('')
                 move = possible_combos[index][winner]
+            else 
+                move
             end
             
        end
-   move
+        move
+    end
+    def get_move(ttt_board)
+
+     win(ttt_board)
+     block(ttt_board)
+    end
+    def win(ttt_board)
+        win_block_move(ttt_board, marker)
+    end
+    def block(ttt_board)
+        win_block_move(ttt_board, opponent)
     end
 end
--- unbeatable_test.rb
+++ unbeatable_test.rb
@@ -6,7 +6,7 @@ class TestUnbeatable < Minitest::Test
     #     assert_equal(2, impossible_var.get_move(['X','X','','','','','','','']))
     # end
     def test_345
-        impossible_var = Unbeatable.new('X')
-        assert_equal(5, impossible_var.get_move(['','','','X','X','','','','']))
+        impossible_var = Unbeatable.new('O')
+        assert_equal(1, impossible_var.get_move(['X','','X','','','','','','']))
     end
 end
